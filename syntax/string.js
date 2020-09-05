de87685330419bc856623d11de9c15428a902e73
SeoJin Kim <sjinjin7@gmail.com>
2020-09-05 19:51:42 +0900

    6.2강 Data type - String
    - note/6.2강 Data type - String.txt 추가
    - syntax/string.js 추가

---
+++ syntax/string.js
@@ -0,0 +1,5 @@
+console.log('1' + '1');
+// 데이터가 문자일시 +는 산술연산자가 아닌 결합 연산자 역할을 진행
+var a = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+console.log(a);
+console.log(a.length);
