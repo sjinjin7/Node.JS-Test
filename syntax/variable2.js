03a337faacd1f35cfe192d10cb46d656bad0494b
SeoJin Kim <sjinjin7@gmail.com>
2020-09-05 20:10:24 +0900

    7.2 변수
    - syntax/variable2.js 추가
    - note/7.2 변수.txt 추가

---
+++ syntax/variable2.js
@@ -0,0 +1,8 @@
+var name = 'egoing';
+// 변수 선은을 통해
+// 1. 용량 줄임
+// 2. 가독성 높임
+// 3. 일괄적으로 변경 가능
+var letter = 'Lorem ' + name + ' ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' + name + ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ' + name;
+
+console.lgo(letter);
