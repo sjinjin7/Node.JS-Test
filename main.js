07254ce251751d95b657deb2e434b5b65d53c839
SeoJin Kim <sjinjin7@gmail.com>
2020-09-05 19:31:12 +0900

    5강 웹서버 만들기

--- main.js
+++ main.js
@@ -2,16 +2,15 @@ var http = require('http');
 var fs = require('fs');
 var app = http.createServer(function(request,response){
     var url = request.url;
-    if(request.url == '/'){
+    if(url == '/'){
       url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(url == '/favicon.ico'){
       response.writeHead(404);
       response.end();
       return;
     }
     response.writeHead(200);
-    console.log(__dirname + url);
     response.end(fs.readFileSync(__dirname + url));
 
 });
