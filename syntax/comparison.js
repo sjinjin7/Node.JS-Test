458876909bdcba837bd51e6fff57d24a9d2ad063
SeoJin Kim <sjinjin7@gmail.com>
2020-09-08 02:07:57 +0900

    14강 Javascript - 비교 연산자
    - syntax/comparison.js 추가 및 코드 추가
    - note/14강 Javascript - 비교 연산자.txt 추가

---
+++ syntax/comparison.js
@@ -0,0 +1,16 @@
+// 지금 까지 배운 연산자
+console.log(1+1);     // 이항 연산자(왼쪽에 있는 항과 오른쪽에 있는 항을 연산 처리하여 하나의 항으로 표현)
+
+// Comparison Operator
+// 비교 연산자는 좌항과 우항을 비교해서 true나 false 둘중 하나의 값을 만들어 내는 연산자
+console.log(1==1);      // 1과 1이 같은가? => true
+console.log(1==2);      // 1과 2가 같은가? => false는
+console.log(1>2);       // false
+console.log(1<2);       // true
+console.log(1===1);     // true
+
+// == 와 ===의 차이
+// == : 좌항 과 우항이 같은가?
+// === : 좌항 과 우항이 정확히 같은가?
+// ex) http://guswnsxodlf.github.io/javascript-equal-operator 참고
+name = 1; // = 는 대입 연산자
