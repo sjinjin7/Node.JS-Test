5a1bf37ad07ed537d843b7e6bf9e96b2de2982ff
SeoJin Kim <sjinjin7@gmail.com>
2020-09-08 02:49:54 +0900

    16강 Javascript - 조건문
    - syntax/conditional.js 추가 및 코드 추가
    - note/16강 Javascript - 조건문.txt 추가

---
+++ syntax/conditional.js
@@ -0,0 +1,18 @@
+console.log('A');
+console.log('B');
+/*
+if(true){
+  console.log('C1');
+}
+if(false){
+  console.log('C2');
+}
+*/
+// 위에것도 가능하고
+if(true){
+  console.log('C1');
+} else{
+  console.log('C2');
+}
+
+console.log('D');
