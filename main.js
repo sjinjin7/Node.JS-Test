5a0d56bf84624b49e38a93448256b1594243ab9d
SeoJin Kim <sjinjin7@gmail.com>
2020-09-07 19:52:45 +0900

    12강 App 제작- 파일을 이용해 본문 구현
    -data 폴더 추가
    - data/CSS, data/HTML, data/JavaScript 추가
    - main.js 코드 추가
    - note/12강 App 제작- 파일을 이용해 본문 구현.txt 추가

--- main.js
+++ main.js
@@ -36,31 +36,35 @@ var app = http.createServer(function(request,response){
     response.writeHead(200);
     //response.end(fs.readFileSync(__dirname + _url));  // 사용자가 접속한 url에 따라서 web2-ndoejs 안에 잇는 파일(1.html 등)을 읽어주는 코드
 
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=Javascript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-      <img src="coding.jpg" width="100%">
-      </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-      </p>
-    </body>
-    </html>
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
 
-    `;
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=Javascript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
 
-    response.end(template); //  queryData 결과 값이 화면에 나옴
+      `;
+
+      response.end(template); //  queryData 결과 값이 화면에 나옴
+
+    });
+
+
+
 
 
 });
