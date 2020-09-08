3adad777bab0bea0f3654eb0fbb0dcdbee147e0a
SeoJin Kim <sjinjin7@gmail.com>
2020-09-09 05:00:08 +0900

    20강 JavaScript - 반복문
    - syntax/loop.js 추가 및 코드 추가
    - note/20강 JavaScript - 반복문.txt 추가

---
+++ syntax/loop.js
@@ -0,0 +1,10 @@
+console.log('A');
+console.log('b');
+
+var i = 0;
+while (i < 10) {  // while의 인자 값이 true인 동안은 코드가 반복적으로 실행
+  console.log('C1');
+  console.log('C2');
+  i = i + 1;
+}
+console.log('D');
