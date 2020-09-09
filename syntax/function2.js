f31c61192a3fa1cf07c7e2215c1d933b11aae561
SeoJin Kim <sjinjin7@gmail.com>
2020-09-09 17:19:58 +0900

    27강 JavaScript - 함수의 출력
    - syntax/function.js 코드 수정
    - note/27강 JavaScript - 함수의 출력.txt 추가

--- syntax/function2.js
+++ syntax/function2.js
@@ -6,9 +6,12 @@ console.log(Math.round(1.4));
 
 // 함수 만들어보기
 // 매개변수가 필요함
-// argument를 전달받아 함수안으로 전달해주는 매게체를 매개변수(parameter)라고 함 
+// argument를 전달받아 함수안으로 전달해주는 매게체를 매개변수(parameter)라고 함
 function sum(first, second){
-  console.log(first+second);
+  console.log(1);
+  return first+second;
+  console.log(2);
 }
 
 sum(2,4); // 각각의 입력값 하나하나를 argument라고 함
+console.log(sum(2,4));
