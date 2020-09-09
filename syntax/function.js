bfb139d021f23884b69382f3ca8258760fe00e54
SeoJin Kim <sjinjin7@gmail.com>
2020-09-09 17:11:56 +0900

    25강 JavaScript - 함수의 기본 문법
    - note/25강 JavaScript - 함수의 기본 문법.txt 추가
    - syntax/function.js 추가 및 코드 추가

---
+++ syntax/function.js
@@ -0,0 +1,27 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log('A');
+console.log('Z');
+console.log('B');
+f123();
+console.log('F');
+console.log('C');
+console.log('P');
+console.log(1);
+console.log(2);
+console.log(3);
+console.log('U');
+console.log('A');
+console.log('Z');
+f123();
+console.log('A');
+console.log('Z');
+console.log('B');
+
+
+function f123(){
+  console.log(1);
+  console.log(2);
+  console.log(3);
+}
