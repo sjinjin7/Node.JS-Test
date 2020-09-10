0cbee1563c151be377a69a7789954e0572621b70
SeoJin Kim <sjinjin7@gmail.com>
2020-09-10 18:11:07 +0900

    41강 JavaScript - 객체의 형식
    - note/41강 JavaScript - 객체의 형식.txt 추가
    - syntax/object.js 추가 및 코드 추가

---
+++ syntax/object.js
@@ -0,0 +1,10 @@
+var members = ['vampa', 'sjin', 'jin']; // 배열은 대괄호
+console.log(members[1]);
+
+var roles = {
+  'programmer':'vampa',
+  'designer' : 'sjin',
+  'manager' : 'jin'
+}  // 객체는 중괄호
+
+console.log(roles.designer);
