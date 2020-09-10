1343196d7abda57daa09febc64f5e401b1fcb9a3
SeoJin Kim <sjinjin7@gmail.com>
2020-09-10 17:52:44 +0900

    39강 App - 글수정 - 삭제버튼 구현
    - main.js 코드 추가 및 수정
    - note/39강 App - 글수정 - 삭제버튼 구현.txt 추가

--- main.js
+++ main.js
@@ -89,7 +89,12 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,`<h2>${title}</h2>
             <p>${description}</p>`,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+            `<a href="/create">create</a>
+             <a href="/update?id=${title}">update</a>
+             <form action="delete_process" method="post" onsubmit="asdf">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">
+             </form>`);
             // update url에 쿼리스트링을 통해서 어떠헌 데이터를 수정할 것인지를 설계
 
             response.writeHead(200);    // 웹브라우저가 웹서버에 접속 했을 때 웹서버가 응. 그때 웹서버와 웹브라우저 사이에서 잘 됫는지 아니면 에러가 있는지 아니면 이페이지기 다른 곳으로 이사를 갓는지 이러한 중요한 정보를 기계와 기계가 통신하기 위한 아주 간결한 약속.
