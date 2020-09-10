6a18a959814322eb3d46f45cc222305291c34a24
SeoJin Kim <sjinjin7@gmail.com>
2020-09-10 18:20:05 +0900

    42강 JavaScript - 객체의 반복
    - note/42강 JavaScript - 객체의 반복.txt 추가
    - syntax/object.js 코드 추가 및 수정

--- syntax/object.js
+++ syntax/object.js
@@ -1,5 +1,11 @@
 var members = ['vampa', 'sjin', 'jin']; // 배열은 대괄호
 console.log(members[1]);
+var i = 0;
+while(i < members.length){
+  console.log('array loop', members[i]);
+  i = i + 1;
+}
+
 
 var roles = {
   'programmer':'vampa',
@@ -8,3 +14,8 @@ var roles = {
 }  // 객체는 중괄호
 
 console.log(roles.designer);
+
+// 변수 name은 객체의 식별자(key)가 나오도록 약속되어 있음
+for(var name in roles){
+  console.log('object => ', name, 'value => ', roles[name]);
+}
