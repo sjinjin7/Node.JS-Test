14849e5290b463f3c132b8a158ba8af8888095c5
SeoJin Kim <sjinjin7@gmail.com>
2020-09-11 03:39:12 +0900

    47강 App 제작 - 모듈의 활용
    - note/47강 App 제작 - 모듈의 활용.txt
    - main.js 코드 수정
    - lib 디렉토리 생성
    - lib/template.js 추가 및 코드 추가

--- main.js
+++ main.js
@@ -3,38 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-var template = {
-  html:function(title, list, body, control){// HTML에 대한 template
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-
-    `;
-  },
-  list:function(filelist){// list에 대한 template에
-    var list = '<ul>';
-    var i = 0;
-    while(i < filelist.length){
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-      i = i + 1;
-    }
-
-    list = list+'</ul>';
-    return list;
-  }
-}
-
+var template = require('./lib/template.js');
 
 // require 요구하다
 // url(fs,http)이라는 것은 요구 한다는 의미
